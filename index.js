87639e20ee44d4cb8da1579fe71eea42a646bd2e
Ana Teixeira <afilipa_teixeira@hotmail.com>
2020-10-10 22:22:16 +0100

    an approach to deal with missing feature in chartjs (help for issue #2)

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
-import { Bar } from 'react-chartjs-2';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import "./style.css";
+import { Bar } from "react-chartjs-2";
 
-require ('./RoundedBars.js')
+require("./RoundedBars.js");
 
 class App extends Component {
   constructor() {
     super();
 
-
-    
     this.state = {
       data: {
-       labels: ["Week 1", "Week 2", "Week 3", "Week 4"],
+        labels: ["Week 1", "Week 2", "Week 3", "Week 4"],
         datasets: [
           {
             label: "Pending Approval",
             backgroundColor: "rgb(51, 153, 255)",
             data: [20, 10, 30, 15],
             stack: 1,
-            name:'AMR'
+            name: "AMR"
           },
           {
             label: "Invoice Created",
             backgroundColor: "rgb(51, 204, 51)",
             data: [60, 20, 20, 30],
-            stack: 1,
-            
+            stack: 1
           },
           {
             label: "Approved",
             backgroundColor: "green",
             data: [40, 50, 20, 45],
-            stack: 1,
-          
+            stack: 1
           },
-           {
+          {
             label: "Pending Approval",
             backgroundColor: "rgb(51, 153, 255)",
             data: [25, 5, 20, 10],
@@ -90,55 +86,94 @@ class App extends Component {
             backgroundColor: "green",
             data: [45, 40, 22, 55],
             stack: 4
-          },
-          
-          
+          }
         ]
       },
 
       options: {
-         cornerRadius: 10,
+        cornerRadius: 10,
         legend: {
           labels: {
             generateLabels: function(chart) {
-              return Chart.defaults.global.legend.labels.generateLabels.apply(this, [chart]).filter(function(item, i){
+              return Chart.defaults.global.legend.labels.generateLabels
+                .apply(this, [chart])
+                .filter(function(item, i) {
                   return i <= 2;
-              });
+                });
             }
           }
         },
         scales: {
-            xAxes: [
-              {
-                id:'xAxis1',
-                type:"category",
-                ticks:{
-                  beginAtZero:true,
-                  callback:function(label, data, index, datasetIndex, labels){
-                    console.log(data)
-                    var region = ['W1 W2 W3 W4'];
-                    return region;
-                  }
+          xAxes: [
+            {
+              id: "xAxis1",
+              type: "category",
+              ticks: {
+                display: false
+              }
+            },
+            {
+              id: "xAxis2",
+              type: "linear",
+              ticks: {
+                beginAtZero: true,
+                max: 4,
+                min: 0,
+                stepSize: 0.25,
+                labelOffset: 15,
+
+                callback: function(value, index, values) {
+                  var array = ["W1", "W2", "W3", "W4"];
+                  // console.log("values:  ", index % array.length) ;
+                  if (index == values.length - 1) return "";
+                  return array[index % array.length];
                 }
-              },
-              
-              {
-                stacked: true
-              },
-            ],
-            yAxes: [{
-                stacked: true
-            }]
+              }
+            },
+            {
+              id: "xAxis3",
+              type: "linear",
+              ticks: {
+                beginAtZero: true,
+                max: 4,
+                min: 0,
+                stepSize: 1,
+                labelOffset: 55,
+                callback: function(value, index, values) {
+                  var weeks = ["Week 1", "Week 2", "Week 3", "Week 4"];
+                  return weeks[index];
+                }
+              }
+            }
+          ],
+          yAxes: [
+            {
+              stacked: true
+            }
+          ]
         },
         tooltips: {
-            mode: 'nearest',
-            callbacks: {
-                    title: function(tooltipItem, data) {
-                      const arr = ['W1', 'W1','W1','W2', 'W2','W2','W3','W3','W3', 'W4','W4','W4']
-                      console.log(tooltipItem)
-                        return arr[tooltipItem[0].datasetIndex]
-                    }
-              }
+          mode: "nearest",
+          callbacks: {
+            title: function(tooltipItem, data) {
+              const arr = [
+                "W1",
+                "W1",
+                "W1",
+                "W2",
+                "W2",
+                "W2",
+                "W3",
+                "W3",
+                "W3",
+                "W4",
+                "W4",
+                "W4"
+              ];
+              // console.log(tooltipItem);
+              return arr[tooltipItem[0].datasetIndex];
+            }
+          }
         }
       }
     };
@@ -147,10 +182,10 @@ class App extends Component {
   render() {
     return (
       <div>
-         <Bar   data={this.state.data} options={this.state.options} />
+        <Bar data={this.state.data} options={this.state.options} />
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
